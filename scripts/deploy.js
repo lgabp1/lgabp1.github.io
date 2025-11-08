e430285911bb78d8486b8b6aa26c4bdb48cd3fa6
lgabp1 <152983873+lgabp1@users.noreply.github.com>
2025-11-08 10:11:46 +0100

    Inherit command output in deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,7 +8,7 @@ import fs from "fs";
     console.log("Starting deploy script...");
 
     /* First, check git status for uncommitted changes */
-    const { stdout } = await execa("git", ["status", "--porcelain"]);
+    const { stdout } = await execa("git", ["status", "--porcelain"], { stdio: 'pipe' });
     if (stdout) {
       throw new Error(
         "You have unstaged or uncommitted changes. Please commit or stash them before deploying."
@@ -16,23 +16,24 @@ import fs from "fs";
     }
 
     /* Check out the gh-pages branch */
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    await execa("git", ["checkout", "--orphan", "gh-pages"], { stdio: "inherit" });
     // eslint-disable-next-line no-console
     
     /* Build */
     console.log("Building started...");
-    await execa("npm", ["run", "build"]);
+    // Stream build output to your terminal
+    await execa("npm", ["run", "build"], { stdio: "inherit" });
     
     /* Understand if it's dist or build folder */
     const folderName = fs.existsSync("dist") ? "dist" : "build";
 
     /* Make the gh-pages branch */
-    await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    await execa("git", ["--work-tree", folderName, "add", "--all"], { stdio: "inherit" });
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"], { stdio: "inherit" });
     
     /* Push to gh-pages */
     console.log("Pushing to gh-pages...");
-    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"], { stdio: "inherit" });
     
     /* Get current os to get the right rm command, and remove folderName */
     const isWindows = process.platform === "win32";
@@ -43,8 +44,8 @@ import fs from "fs";
     }
 
     /* Reset to master and delete the local gh-pages branch */
-    await execa("git", ["checkout", "-f", "master"]);
-    await execa("git", ["branch", "-D", "gh-pages"]);
+    await execa("git", ["checkout", "-f", "master"], { stdio: "inherit" });
+    await execa("git", ["branch", "-D", "gh-pages"], { stdio: "inherit" });
     console.log("Successfully deployed, check your settings");
   } catch (e) {
     // eslint-disable-next-line no-console
